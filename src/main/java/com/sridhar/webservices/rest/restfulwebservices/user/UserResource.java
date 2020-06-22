18f7f850e60d6aadf872bffb984bd0f9cc2f6e1d
Sridhar Vellore <sridhar.vellore2@gmail.com>
2020-06-21 22:10:05 -0400

    Hateoas Support

--- src/main/java/com/sridhar/webservices/rest/restfulwebservices/user/UserResource.java
+++ src/main/java/com/sridhar/webservices/rest/restfulwebservices/user/UserResource.java
@@ -6,8 +6,9 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -33,12 +34,16 @@ public class UserResource {
 	}
 
 	@GetMapping("/{id}")
-	public User findUserById(@PathVariable int id) {
+	public EntityModel<User> findUserById(@PathVariable int id) {
 		User user = service.findById(id);
 		if (user == null)
 			throw new UserNotFoundException("[id:" + id + "]");
-
-		return user;
+		
+		EntityModel<User> resource = EntityModel.of(user);
+		WebMvcLinkBuilder linkTo = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(UserResource.class).getAllUsers());
+		resource.add(linkTo.withRel("all-users"));
+		
+		return resource;
 	}
 
 	@PostMapping()
